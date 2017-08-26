848d4c827c745bc8f7478de71c2a6cc20a72f77b
Clayton Blythe <blythec1@central.edu>
2017-08-26 00:39:44 -0400

    =working site with customized pelican theme based off elegant forest green color

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Clayton Blythe'
-SITENAME = 'Deep Python'
+SITENAME = u""" <span style="color:#254601;">--Deep Python--</span>"""
 SITEURL = ''
 
 PATH = 'content'
@@ -21,13 +21,11 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Python.org', 'http://python.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('LinkedIn', 'http://linkedin.com/claytonblythe'),
+          ('Github', 'http://github.com/claytonblythe'),)
 
 DEFAULT_PAGINATION = 7
 
@@ -38,14 +36,16 @@ THEME="/users/claytonblythe/Desktop/Mega/Data_Science/projects/pelican-themes/el
 
 LANDING_PAGE_ABOUT = {'title': 'Deep Python:  Using Python for Deep Learning',
         'details': """<div itemscope itemtype="http://schema.org/Person"><p>My name
-        is <span itemprop="name">Clayton Blythe</span>.<p>I work at Ford Motor Company <a href="http://ford.com/"
+        is <span itemprop="name">Clayton Blythe</span>.<p>I work at <a href="http://ford.com/"
         title="Ford Motor Company" itemprop="affiliation">Ford Motor Company</a> as a Machine Learning / Artificial Intelligence scientist. I play a broad role there, which
         includes research, product development, engineering and deployment for a variety of data intensive projects such as cybersecurity, customer interaction,
         and autonomous vehicle detection initiatives.
-        </p>You can find my github profile at <a href="https://github.com/claytonblythe/" title="My Github
-       profile" itemprop="url"><span itemprop="nickname">github.com/claytonblythe</span></a>  You can also reach me
-       via my personal email at <a
+        </p>You can take a look at my <a href="https://github.com/claytonblythe/" title="My Github
+       profile" itemprop="url"><span itemprop="nickname">Github profile</span></a>, reach out to me
+       at my personal email <a
        href="mailto:claytondblythe@gmail.com" title="My email
-       address" itemprop="email">claytondblythe@gmail.com</a>.</p><p>I mostly work in Python, OSX and Linux, I also occasionally dabble in other languages like R, Scala, and Go. </p><p>Besides programming, I like to exercise, go to
-       live concerts, tinker with my raspberry pi, drink beer, and play guitar.
+       address" itemprop="email">claytondblythe@gmail.com</a>, or mesage me on <a
+       href="http://linkedin.com/in/claytonblythe" title="My linkedin
+       profile" itemprop="linkedin">LinkedIn.</a></p><p>I mostly work in Python within OSX and Linux, but I have also occasionally dabbled in other languages like R, Scala, and Go. </p><p>Besides programming, I like to exercise, go to
+       live concerts, tinker with my raspberry pi testingtesting projects, drink beer, and play guitar.
        </p></div>"""}
